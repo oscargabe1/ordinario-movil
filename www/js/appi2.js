3b58d2a53d5d59effc7bd8e063b8258d8f10f41a
Oscar Gaytan <oscargabe1@gmail.com>
2017-05-31 18:17:04 -0500

    ordinario de Oscar, Franco y Rafa

--- www/js/appi2.js
+++ www/js/appi2.js
@@ -9,6 +9,14 @@ $(function() {
 	};
 
 
+	function menu_categoria(categoria){
+		return $.ajax({
+			url: 'http://ipm.esy.es/public/menu/'+categoria,
+			type: 'GET',
+			dataType: 'json'
+		});
+	};
+
 
 
 
@@ -17,35 +25,61 @@ $(function() {
 
 	ViewModel = function(){
 		var _self = this;
+		var temp=[];
 
-		_self.menu_producto = ko.observableArray([]);
 		_self.producto_pedido = ko.observableArray([]);
 
 
 		
-				CargarTODOmenu().done(function(res){
-					if(res.error){
-						console.log(res.msg);
-						return false;
-					}
-					//localStorage.setItem("id_comida", res.datos.ID);
-					//localStorage.setItem("Categoria", res.datos.Categoria);
-					//localStorage.setItem("Categoria", res.datos.ID);
-					//NO SE QUE ONDA CON LA DISPONIBILIDAD DEBERIA SER VALIDADO EN EL API
-					console.log(res.datos);
-					_self.producto_pedido(res.datos);
-				});
+				// CargarTODOmenu().done(function(res){
+				// 	if(res.error){
+				// 		console.log(res.msg);
+				// 		return false;
+				// 	}
+				// 	//localStorage.setItem("id_comida", res.datos.ID);
+				// 	//localStorage.setItem("Categoria", res.datos.Categoria);
+				// 	//localStorage.setItem("Categoria", res.datos.ID);
+				// 	//NO SE QUE ONDA CON LA DISPONIBILIDAD DEBERIA SER VALIDADO EN EL API
+				// 	console.log(res.datos);
+				// 	_self.producto_pedido(res.datos);
+				// });
 		
 
-		_self.AgregarPedido = function(datos){
-			var temp=[];
-			if(data.Categoria=="rico")
-				temp[0]=data;
-			if(data.Categoria=="masriko")
-				temp[1]=data;
-			if(data.Categoria=="sabrosos")
-				temp[2]=data;
-			_self.producto_pedido(temp);
+		_self.guardarPEDIDO = function(datos){
+			
+			if(data.Categoria=="comida")
+				temp[0]=datos;
+			if(data.Categoria=="bebida")
+				temp[1]=datos;
+			if(data.Categoria=="postre")
+				temp[2]=datos;
+			console.log(temp);
+		}
+
+		_self.comidita= function(datos){
+
+				menu_categoria("comida").done(function(res){
+					_self.producto_pedido(res.datos);
+
+
+				});
+		}
+
+		_self.bebida= function(datos){
+
+				menu_categoria("bebida").done(function(res){
+					_self.producto_pedido(res.datos);
+
+
+				});
+		}
+		_self.postres= function(datos){
+
+				menu_categoria("postre").done(function(res){
+					_self.producto_pedido(res.datos);
+
+
+				});
 		}
 
 
